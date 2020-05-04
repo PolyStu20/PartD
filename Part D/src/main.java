e403c29a3874ce7ecbc83c2583d2bd34428ce9f1
PolyStu20 <cepnz@kinect.co.nz>
2020-05-05 08:22:44 +1200

    2nd line done

--- Part D/src/main.java
+++ Part D/src/main.java
@@ -4,6 +4,7 @@ public class main {
 	public static void main(String[] args) {
 		// assignment 2 Part D. Adding lines and committing changes
 			System.out.println("Hi, my name is Stuart");
+			System.out.println("My favourite colour is blue");
 		
 	}
 
